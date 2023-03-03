1f3888fdecbb44f4d7ae58be80dc4b39bcca8b7f
kilativ147 <90765525+kilativ147@users.noreply.github.com>
2023-03-03 18:05:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ if (burgerContainer){
     }
   }
 }
+
+//Функція відкриття та закриття вікна з запитом
+window.onload = function() {
+	//додаємо усі задіяні кнопки
+  var buttons = document.querySelectorAll(".button-red, .request-close");
+  //первірка усіх кнопок на "клік"
+	for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(event) {
+			//шукаємо вікно якому необхідно додати клас
+      var formRequest = document.querySelector('.form__request');
+			var formArea = document.querySelector('.request__form-row');
+			if (formRequest && formArea)
+			{
+				formRequest.classList.toggle('_active');
+				formArea.classList.toggle('_active');
+				document.body.classList.toggle('._lock');	
+				//зчитуємо атрибут data-param що прописаний у HTML в кожній задіяній кнопці
+				var value = this.getAttribute("data-param");				
+				//Отримуєм ОБ
+				let textElement = document.getElementById(`request_topic`);
+				//	Змінюємо вміст тегу
+				textElement.innerHTML = value;
+			}
+    });
+  }
+}
+
+//Курси
+  
+
